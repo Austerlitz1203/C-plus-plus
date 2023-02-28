648f6468adcc6f55b7bc409b7656daf6f5e473ef
Austerlitz-0 <1477567612@qq.com>
2023-02-28 17:28:56 +0800

    完善实现string类

--- String/String/Test.cpp
+++ String/String/Test.cpp
@@ -10,8 +10,76 @@ void Test1()
 	cout<< s2.getstr() << endl;
 }
 
+void Test2()
+{
+	simulate::string s1("Hello String!");
+	cout << s1[4] << endl;
+
+	for (size_t i = 0; i < s1.size(); i++)
+	{
+		cout << s1[i];
+	}
+	cout << endl;
+
+	simulate::string s2("who am i");
+	cout << (s1 <= s2) << endl;
+	cout << (s1 == s2) << endl;
+	cout << (s1 > s2) << endl;
+
+	for (auto ch : s2) // 范围 for 其实就是自己写了 begin() ，就可以用了。但是注意，函数名必须是begin，小写
+	{
+		cout << ch << " ";
+	}
+}
+
+void Test3()
+{
+	simulate::string s1("Hello");
+	s1.pushback(' ');
+	s1.pushback('W');
+	s1.pushback('o');
+	cout << s1.getstr() << endl;
+	cout << s1.size() << endl;
+	s1.append("rld");
+	cout << s1.getstr() << endl;
+	cout << s1.size() << endl;
+}
+
+void Test4()
+{
+	simulate::string s1;
+	s1 += 'H';
+	s1 += 'l';
+	s1 += "lo";
+	cout << s1.getstr() << endl;
+
+	s1.insert(2, 'a');
+	s1.insert(0, 'l');
+	s1.insert(s1.size(), 'e');
+	cout << s1.getstr() << endl;
+
+	simulate::string s2("Helrld!");
+	s2.insert(3, "lo wo");
+	cout << s2.getstr() << endl;
+
+	simulate::string s3("where");
+	s3.insert(0, "who ");
+	s3.insert(s3.size(), " are you");
+	cout << s3.getstr() << endl;
+
+	simulate::string s4("This is an example sentence");
+	s4.erase(10, 8);
+	s4.erase(9, 1);
+	s4.erase(0, 5);
+	cout << s4.size() << endl;
+	s4.erase(s4.size(), 1);
+	cout << s4.getstr() << endl;
+}
+
 int main()
 {
-	Test1();
-
+	//Test1();
+	//Test2();
+	//Test3();
+	Test4();
 }
--- String/String/string.h
+++ String/String/string.h
@@ -2,6 +2,7 @@
 #pragma once
 #include<iostream>
 #include<algorithm>
+#include<assert.h>
 
 
 
@@ -11,21 +12,44 @@ namespace simulate
 	class string
 	{
 	public:
+		typedef char* iterator;
+		typedef const char* const_iterator;
+
+
+		const_iterator begin()const
+		{
+			return _str;
+		}
+
+		const_iterator end()const
+		{
+			return _str + _size;
+		}
+
+		iterator begin()
+		{
+			return _str;
+		}
+
+		iterator end()
+		{
+			return _str + _size;
+		}
 
 		// string(const char* s = nullptr) 是不可以的，传参 '\0' 也不可以。因为cout对于char*类型的，是一直读到 \0 为止
 		string(const char* s = "")
 			:_size(strlen(s))    // 如果同时初始化_capacity，会导致按照申明的顺序初始化，顺序改动会造成随机值
 		{
-			_str = new char[_size + 1];
-			_capacity = _size + 1;
+			_capacity = _size == 0 ? 3 : _size + 1;
+			_str = new char[_capacity+1];
 			strcpy(_str, s);
 		}
 
 		string(const string& s)
-			:_size(s._size)    
-			,_capacity(s._capacity)
+			:_size(s._size)
+			, _capacity(s._capacity)
 		{
-			_str = new char[_capacity+1];
+			_str = new char[_capacity + 1];
 			strcpy(s._str, _str);
 		}
 
@@ -38,7 +62,7 @@ namespace simulate
 				//_size = s._size;
 				//_capacity = s._capacity + 1;
 
-				char* tmp = new char[s._capacity+1];
+				char* tmp = new char[s._capacity + 1];
 				strcpy(tmp, s._str);
 				_str = tmp;
 				_size = s._size;
@@ -47,15 +71,168 @@ namespace simulate
 			return *this;
 		}
 
-		char* getstr()
+		char* getstr()const
 		{
 			return _str;
 		}
 
+		char& operator[](size_t pos)
+		{
+			assert(pos < _size);
+			return _str[pos];
+		}
+
+		const char& operator[](size_t pos)const
+		{
+			assert(pos < _size);
+			return _str[pos];
+		}
+
+		int size()const
+		{
+			return _size;
+		}
+
+		bool operator>(const string& s)const
+		{
+			return strcmp(_str, s._str) > 0;
+		}
+
+		bool operator==(const string& s)const
+		{
+			return strcmp(_str, s._str) == 0;
+		}
+
+		bool operator>=(const string& s)const
+		{
+			return (*this > s) || (s == *this); // 如果不设为const，这样会报错，因为 s 调用 == ，静态成员调用非静态成员函数
+		}
+
+		bool operator<(const string& s)const
+		{
+			return !(*this >= s);
+		}
+
+		bool operator<=(const string& s)const
+		{
+			return !(*this > s);
+		}
+
+		void reserve(int n) // 扩容，不改变数据
+		{
+			char* tmp = new char[n + 1];
+			strcpy(tmp, _str);
+			delete[] _str;
+			_str = tmp;
+			_capacity = n;
+		}
+
+		void pushback(char ch)
+		{
+			if (_size + 1 > _capacity)
+			{
+				reserve(_capacity*2);
+			}
+
+			_str[_size] = ch;
+			_size++;
+			_str[_size] = '\0';
+		}
+
+		void append(const char* str)
+		{
+			int len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				reserve(_size + len);
+			}
+			strcpy(_str + _size, str);  // 如果使用strcat，这个函数需要自己去找 \0 ,比较浪费时间
+			_size += len;
+		}
+
+		string& operator+=(const char ch)
+		{
+			pushback(ch);
+			return *this;
+		}
+
+		string& operator+=(const char* str)
+		{
+			append(str);
+			return *this;
+		}
+
+		void insert(int pos, const char ch)
+		{
+			assert(pos <= _size);
+			if (_size + 1 > _capacity)
+			{
+				reserve(_capacity * 2);
+			}
+			size_t end = _size;
+			for (int i = end - 1; i >= pos; i--)
+			{
+				_str[i + 1] = _str[i];
+			}
+			_str[pos] = ch;
+			_size++;
+			_str[_size] = '\0';
+		}
+
+		void insert(int pos, const char* str)
+		{
+			assert(pos <= _size);
+			int len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				reserve(_size + len);
+			}
+			size_t end = _size;
+			for (int i = end - 1; i >= pos; i--) // 后移
+			{
+				_str[i + len] = _str[i];
+			}
+
+			const char* tmp = str;
+			for (int i = pos; i < pos+len; i++) // 插入
+			{
+				_str[i] = *str++;
+			}
+			_size += len;
+			_str[_size] = '\0';
+
+		}
+
+		void erase(size_t pos, size_t len = npos)
+		{
+			assert(pos <= _size && npos > 0);
+			for (int i = pos; i + len < _size; i++)
+			{
+				_str[i] = _str[i + len];
+			}
+			_size -= len;
+			_str[_size] = '\0';
+		}
+
+		~string()
+		{
+			delete[] _str;
+			_str = nullptr;
+			_size = 0;
+			_capacity = 0;
+		}
+
 	private:
 		char* _str;
-		int _size;
-		int _capacity;
+		int _size;      // 目前的长度，不包括 \0
+		int _capacity;  // 最大容量，不包括最后一个 \0
+
+		static size_t npos; // 不可以用缺省值（初始化列表使用），因为static修饰，是全局变量
+
+		//static const size_t npos1 = -1;  // 但是这样子就可以（只对整形有效），所以用起来要注意。
+		//static const double npos2 = 1.9; // 不可以，因为不是整形
 	};
+
+	size_t string::npos = -1;
 
 }
