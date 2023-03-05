c76d4085b5ebd786e473979bab1ebc47fb018d9c
Austerlitz-0 <1477567612@qq.com>
2023-03-05 13:38:12 +0800

    模拟实现 vector

--- vector/vector/test.cpp
+++ vector/vector/test.cpp
@@ -1,4 +1,5 @@
 #include"vector.h"
+#include<algorithm>
 
 void Test1()
 {
@@ -32,9 +33,106 @@ void Test2()
 
 }
 
+
+void Test3()
+{
+	simulate::vector<int> v1(10,5);
+	simulate::vector<int>::iterator it = v1.begin();
+
+	for (int i = 0; i < v1.size(); i++)
+	{
+		cout << *it << " ";
+		it++;
+	}
+	cout << endl;
+
+	for (auto v : v1)
+	{
+		cout << v << " ";
+	}
+	cout << endl;
+
+	std::string s("hello world!");
+	simulate::vector<int> s1(s.begin(),s.end());
+	for (auto c : s1)
+	{
+		cout << c << " ";
+	}
+	cout << endl;
+
+	int arr[10] = { 5, 2, 7, 9, 10, 22, 6, 3, 1, 0 };
+	simulate::vector<int> v2(arr, arr + 10);
+	for (auto v : v2)
+	{
+		cout << v << " ";
+	}
+	cout << endl;
+
+	sort(v2.begin(), v2.end());
+	for (auto v : v2)
+	{
+		cout << v << " ";
+	}
+	cout << endl;
+
+	for (auto a : arr)
+	{
+		cout << a << " ";
+	}
+	cout << endl;
+
+	sort(arr, arr + sizeof(arr) / sizeof(int));
+	for (auto a : arr)
+	{
+		cout << a << " ";
+	}
+	cout << endl;
+}
+
+// 删除vector 里面的偶数,用迭代器，会出现迭代器失效的情况
+void Test4()
+{
+	simulate::vector<int> v1;
+	v1._push_back(10);
+	v1._push_back(2);
+	v1._push_back(1);
+	v1._push_back(4);
+	v1._push_back(7);
+	v1._push_back(6);
+
+	simulate::vector<int>::iterator it = v1.begin();
+	//while (it != v1.end())
+	//{
+	//	if (*it % 2 == 0)
+	//	{
+	//		v1.erase(it);
+	//	}
+	//	++it;
+	//}
+
+	// 上面会报错的本质原因就是——删除之后，后面的元素 前移，当前的指针其实相当于后移，但是下面有后移一次，相当于跳过一个元素。
+	// 如果最后一个元素也是偶数，要删除，相当于跳过了 _finish 指针，无限循环
+	while (it != v1.end())
+	{
+		if (*it % 2 == 0)
+		{
+			it=v1.erase(it);
+		}
+		else ++it;
+	}
+
+	for (auto v : v1)
+	{
+		cout << v << " ";
+	}
+	cout << endl;
+}
+
 int main()
 {
-	Test1();
-	Test2();
+	//Test1();
+	//Test2();
+	//Test3();
+	Test4();
 	return 0;
 }
--- vector/vector/vector.h
+++ vector/vector/vector.h
@@ -13,11 +13,60 @@ namespace simulate
 		typedef T* iterator;
 		typedef const T* const_iterator;
 
-		vector()
-			:_start(nullptr)
-			,_finish(nullptr)
-			,_end_of_storage(nullptr)
-		{}
+		//vector()
+		//	:_start(nullptr)
+		//	,_finish(nullptr)
+		//	,_end_of_storage(nullptr)
+		//{}
+
+		//vector(size_t n, const T& val=T())
+		//	:_start(nullptr)
+		//	,_finish(nullptr)
+		//	,_end_of_storage(nullptr)
+		//{
+		//	reserve(n);
+		//	iterator* tmp = _start;
+		//	for (size_t i = 0; i < n; i++)
+		//	{
+		//		*tmp = val;
+		//		tmp++;
+		//	}
+		//}
+
+		// 直接用缺省值替代初始化列表
+		vector(){}
+
+		vector(size_t n, const T& val = T())
+		{
+			reserve(n);
+			for (size_t i = 0; i < n; i++)
+			{
+				*(_start + i) = val;
+				++_finish;  // 不要忘了++ _finish ，一开始忘记了，所以打印不出来
+			}
+		}
+
+		vector(int n, const T& val = T())
+		{
+			reserve(n);
+			for (size_t i = 0; i < n; i++)
+			{
+				*(_start + i) = val;
+				++_finish;  // 不要忘了++ _finish ，一开始忘记了，所以打印不出来
+			}
+		}
+
+		// 写了模板，但是不加上上面的int参数重载，会报错，因为vector(10,5)会调用模板函数
+		template<class InputIterator>
+		vector(InputIterator begin, InputIterator end)
+		{
+			reserve(end - begin);
+			while (begin != end)
+			{
+				_push_back(*begin);
+				++begin;
+			}
+		}
 
 		iterator begin()
 		{
@@ -74,7 +123,7 @@ namespace simulate
 			}
 			else
 			{
-				if (n > apacity())
+				if (n > capacity())
 				{
 					reserve(n);
 				}
@@ -124,11 +173,56 @@ namespace simulate
 			--_finish;
 		}
 
+		// 用迭代器，可能会出现迭代器失效的情况
+		iterator insert(iterator pos , const T& x)
+		{
+			assert(pos >= _start);
+			assert(pos < _finish);
+			size_t len = pos - _start;
+			if (_finish == _end_of_storage)
+			{
+				reserve(2 * capacity());
+				pos = _start + len;  // 扩容之后，_start 改变了
+			}
+
+			iterator tmp = _finish - 1;
+			while (tmp >= pos)
+			{
+				*(tmp + 1) = *tmp;
+				--tmp;
+			}
+			_start[pos] = x;
+			_finish++;
+
+			return pos;
+		}
+
+		iterator erase(iterator pos)
+		{
+			assert(pos >= _start);
+			assert(pos < _finish);
+			iterator tmp = pos + 1;
+
+			while (tmp != _finish)
+			{
+				*(tmp-1) = *tmp;
+				tmp++;
+			}
+			--_finish;
+
+			return pos;
+		}
+
+		~vector()
+		{
+			delete[] _start;
+			_finish = _end_of_storage = nullptr;
+		}
 
 	private:
-		T* _start;
-		T* _finish;
-		T* _end_of_storage;
+		T* _start=nullptr;
+		T* _finish=nullptr;
+		T* _end_of_storage=nullptr;
 	};
 
 }
