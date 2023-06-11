ae9fffbfc61a809f4d45531a1c1ff485d365ef94
Austerlitz-0 <1477567612@qq.com>
2023-06-11 12:13:22 +0800

    lambda表达式、线程库

--- C++11/C++11/Test.cpp
+++ C++11/C++11/Test.cpp
@@ -5,6 +5,8 @@
 #include<map>
 #include<string>
 #include<assert.h>
+#include<algorithm>
+#include<thread>
 
 using namespace std;
 
@@ -465,46 +467,403 @@ private:
 #include "Date.h"
 
 // STL容器的插入接口都有一个emplace系列
+//int main()
+//{
+//	// 深拷贝的类
+//	std::list<simulate::string> mylist;
+//	// 没区别
+//	/*bit::string s1("1111");
+//	mylist.push_back(s1);
+//	mylist.emplace_back(s1);
+//
+//	cout << endl;
+//	bit::string s2("2222");
+//	mylist.push_back(move(s1));
+//	mylist.emplace_back(move(s2));*/
+//
+//	// 开始有区别
+//	//cout << endl;
+//	//mylist.push_back("3333");   // 构造匿名对象 + 移动构造
+//	//mylist.emplace_back("3333");// 直接构造
+//
+//	// 浅拷贝的类
+//	// 没区别
+//	std::list<Date> list2;
+//	Date d1(2023, 5, 28);
+//	list2.push_back(d1);
+//	list2.emplace_back(d1);
+//
+//	cout << endl;
+//	Date d2(2023, 5, 28);
+//	list2.push_back(move(d1));
+//	list2.emplace_back(move(d2));
+//
+//	// 有区别
+//	cout << "=========================" << endl;
+//	list2.push_back(Date(2023, 5, 28));
+//	list2.push_back({ 2023, 5, 28 });
+//
+//	cout << endl;
+//	list2.emplace_back(Date(2023, 5, 28)); // 构造+移动构造
+//	list2.emplace_back(2023, 5, 28);       // 直接构造
+//
+//
+//	return 0;
+//}
+
+
+
+
+struct Goods
+{
+	string _name;  // 名字
+	double _price; // 价格
+	int _evaluate; // 评价
+
+	Goods(const char* str, double price, int evaluate)
+		:_name(str)
+		, _price(price)
+		, _evaluate(evaluate)
+	{}
+};
+
+struct ComparePriceLess
+{
+	bool operator()(const Goods& gl, const Goods& gr)
+	{
+		return gl._price < gr._price;
+	}
+};
+
+struct ComparePriceGreater
+{
+	bool operator()(const Goods& gl, const Goods& gr)
+	{
+		return gl._price > gr._price;
+	}
+};
+
+
+
+//int main()
+//{
+//	vector<Goods> v = { { "苹果", 2.1, 5 }, { "香蕉", 3, 4 }, { "橙子", 2.2, 3 }, { "菠萝", 1.5, 4 } };
+//	for (auto &a : v)
+//	{
+//		cout << a._name <<":"<<a._price << endl;
+//	}
+//	cout << "---------------------------" << endl;
+//
+//	// <
+//	sort(v.begin(), v.end(), ComparePriceLess());
+//
+//	for (auto& a : v)
+//	{
+//		cout << a._name << ":" << a._price << endl;
+//	}
+//
+//	cout << "---------------------------" << endl;
+//	// >
+//	sort(v.begin(), v.end(), ComparePriceGreater());
+//
+//	for (auto& a : v)
+//	{
+//		cout << a._name << ":" << a._price << endl;
+//	}
+//
+//
+//
+//	// lambda 表达式
+//	auto add1 = [](int x, int y)->int {return x + y; };
+//
+//	//cout << [](int x, int y)->int {return x + y; }(1, 2) << endl;
+//	cout << add1(1, 2) << endl;
+//
+//	auto add2 = [](int x, int y) 
+//	{
+//		return x + y;
+//	};
+//
+//	cout << add2(1, 2) << endl;
+//
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	vector<Goods> v = { { "苹果", 2.1, 5 }, { "香蕉", 3, 4 }, { "橙子", 2.2, 3 }, { "菠萝", 1.5, 4 } };
+//
+//	auto priceLess = [](const Goods& g1, const Goods& g2)->bool {return g1._price < g2._price; };
+//	sort(v.begin(), v.end(), priceLess);
+//
+//	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2)->bool {
+//		return g1._price > g2._price; 
+//		});
+//
+//	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2)->bool {
+//		return g1._evaluate < g2._evaluate;
+//		});
+//
+//	sort(v.begin(), v.end(), [](const Goods& g1, const Goods& g2)->bool {
+//		return g1._evaluate > g2._evaluate;
+//		});
+//}
+
+
+
+
+//int main()
+//{
+//	int x = 0, y = 1;
+//	int m = 0, n = 1;
+//
+//	auto swap1 = [](int& rx, int& ry)
+//	{
+//		int tmp = rx;
+//		rx = ry;
+//		ry = tmp;
+//	};
+//
+//	swap1(x, y);
+//	cout << x << " "<< y << endl;
+//
+//	// 传值捕捉
+//	/*auto swap2 = [x, y]() mutable 
+//	{
+//		int tmp = x;
+//		x = y;
+//		y = tmp;
+//	};
+//
+//	swap2();
+//	cout << x << " " << y << endl;*/
+//
+//	// 引用捕捉
+//	auto swap2 = [&x, &y]()
+//	{
+//		int tmp = x;
+//		x = y;
+//		y = tmp;
+//	};
+//
+//	swap2();
+//	cout << x << " " << y << endl;
+//
+//	// 混合捕捉
+//	auto func1 = [&x, y]()
+//	{
+//		//...
+//	};
+//
+//	// 全部引用捕捉
+//	auto func2 = [&]()
+//	{
+//		//...
+//	};
+//
+//	// 全部传值捕捉
+//	auto func3 = [=]()
+//	{
+//		//...
+//	};
+//
+//	// 全部引用捕捉，x传值捕捉
+//	auto func4 = [&, x]()
+//	{
+//		//...
+//	};
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+void Func(int n, int num)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout <<num<<":" << i << endl;
+	}
+	cout << endl;
+}
+
+//int main()
+//{
+//	int n1, n2;
+//	cin >> n1>>n2;
+//	thread t1(Func, n1, 1);
+//	thread t2(Func, n2, 2);
+//
+//	t1.join();
+//	t2.join();
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n1, n2;
+//	cin >> n1 >> n2;
+//	thread t1([n1](int num)
+//		{
+//			for (int i = 0; i < n1; i++)
+//			{
+//				cout <<num<<":" << i << endl;
+//			}
+//			cout << endl;
+//		}, 1);
+//
+//	thread t2([n2](int num)
+//		{
+//			for (int i = 0; i < n2; i++)
+//			{
+//				cout << num << ":" << i << endl;
+//			}
+//			cout << endl;
+//		}, 2);
+//
+//	t1.join();
+//	t2.join();
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	size_t m;
+//	cin >> m;
+//	vector<thread> vthds(m);
+//
+//	// 要求m个线程分别打印n
+//	for (size_t i = 0; i < m; i++)
+//	{
+//		size_t n;
+//		cin >> n;
+//
+//		vthds[i] = thread([i, n, m]() {
+//			for (int j = 0; j < n; j++)
+//			{
+//				cout << i << ":" << j << endl;
+//			}
+//			cout << endl;
+//			});
+//	}
+//
+//	for (auto& t : vthds)
+//	{
+//		t.join();
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+
+class Rate
+{
+public:
+	Rate(double rate) : _rate(rate)
+	{}
+
+	double operator()(double money, int year)
+	{
+		return money * _rate * year;
+	}
+
+private:
+	double _rate;
+};
+
+
+
+//int main()
+//{
+//	/*int x = 0, y = 1;
+//	int m = 0, n = 1;
+//
+//	auto swap1 = [](int& rx, int& ry)
+//	{
+//		int tmp = rx;
+//		rx = ry;
+//		ry = tmp;
+//	};
+//
+//	cout << sizeof(swap1) << endl;*/
+//
+//	// 函数对象
+//	double rate = 0.49;
+//	Rate r1(rate);
+//	r1(10000, 2);
+//
+//	// lambda
+//	auto r2 = [=](double money, int year)->double {return money * rate * year; };
+//	r2(10000, 2);
+//
+//	auto f1 = [] {cout << "hello world" << endl; };
+//	auto f2 = [] {cout << "hello world" << endl; };
+//
+//	//f1 = f2;
+//
+//	return 0;
+//}
+
+
+
+
+
 int main()
 {
-	// 深拷贝的类
-	std::list<simulate::string> mylist;
-	// 没区别
-	/*bit::string s1("1111");
-	mylist.push_back(s1);
-	mylist.emplace_back(s1);
+	size_t m;
+	cin >> m;
+	vector<thread> vthds(m);
 
-	cout << endl;
-	bit::string s2("2222");
-	mylist.push_back(move(s1));
-	mylist.emplace_back(move(s2));*/
+	// 要求m个线程分别打印n次
+	for (size_t i = 0; i < m; i++)
+	{
+		size_t n = 10;
+		vthds[i] = thread([n, m]() {
+			for (int j = 0; j < n; j++)
+			{
+				cout << this_thread::get_id() << ":" << j << endl;
 
-	// 开始有区别
-	//cout << endl;
-	//mylist.push_back("3333");   // 构造匿名对象 + 移动构造
-	//mylist.emplace_back("3333");// 直接构造
+				this_thread::sleep_for(chrono::milliseconds(200));
+			}
+			cout << endl;
 
-	// 浅拷贝的类
-	// 没区别
-	std::list<Date> list2;
-	Date d1(2023, 5, 28);
-	list2.push_back(d1);
-	list2.emplace_back(d1);
+			});
 
-	cout << endl;
-	Date d2(2023, 5, 28);
-	list2.push_back(move(d1));
-	list2.emplace_back(move(d2));
+		//cout << vthds[i].get_id() << endl;
+	}
 
-	// 有区别
-	cout << "=========================" << endl;
-	list2.push_back(Date(2023, 5, 28));
-	list2.push_back({ 2023, 5, 28 });
+	for (auto& t : vthds)
+	{
+		t.join();
+	}
 
-	cout << endl;
-	list2.emplace_back(Date(2023, 5, 28)); // 构造+移动构造
-	list2.emplace_back(2023, 5, 28);       // 直接构造
+	int i = 0;
+	//i += 1;
 
+	// 原子
+	/*int old = i;
+	while (!__sync_bool_compare_and_swap(&i, old, old+1))
+	{
+		old = i;
+	}*/
 
 	return 0;
 }
