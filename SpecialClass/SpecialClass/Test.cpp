2a34694f7f5aedfd94837d9711c409cbf015e1ed
Austerlitz-0 <1477567612@qq.com>
2023-07-12 11:42:15 +0800

    C++的IO流

--- SpecialClass/SpecialClass/Test.cpp
+++ SpecialClass/SpecialClass/Test.cpp
@@ -3,6 +3,8 @@
 #include<mutex>
 #include<vector>
 #include<string>
+#include<set>
+#include<functional>
 using namespace std;
 
 
@@ -393,10 +395,402 @@ private:
 };
 
 
+//int main()
+//{
+//	Singleton::GetInstance();
+//	Singleton::GetInstance();
+//
+//	return 0;
+//}
+
+
+
+
+// C++ 的类型转换
+
+void Test()
+{
+	int i = 1;
+	// 隐式类型转换
+	double d = i;
+	printf("%d, %.2f\n", i, d);
+
+	int* p = &i;
+	// 显示的强制类型转换
+	int address = (int)p;
+
+	printf("%x, %d\n", p, address);
+}
+
+
+// 如下，以前写代码往往面临一些问题，pos == 0时就会有错误，也是因为类型转换
+//void insert(size_t pos, char ch)
+//{
+//	int end = _size;
+//	while (end >= pos)
+//	{
+//		//...
+//		--end;
+//	}
+//}
+
+
+//int main()
+//{
+//	double d = 12.34;
+//	int a = static_cast<int>(d);
+//	cout << a << endl;
+//
+//	// 这里使用static_cast会报错，应该使用reinterpret_cast
+//	//int *p = static_cast<int*>(a);
+//	int *p = reinterpret_cast<int*>(a);
+//
+//	return 0;
+//}
+
+
+//void main()
+//{
+//	volatile const int a = 2;
+//	//int* p = const_cast<int*>(&a);
+//	int* p = (int*)&a;
+//
+//	*p = 3;
+//
+//
+//	// 有没有修改到a ，是否要加上 volatile
+//	cout << a << endl;
+//	cout << *p << endl;
+//}
+
+
+class A
+{
+public:
+	virtual void f(){}
+};
+
+class B : public A
+{};
+
+
+
+void fun(A* pa, const string& s)
+{
+	cout <<"pa指向"<<s << endl;
+
+	// dynamic_cast会先检查是否能转换成功，能成功则转换，不能则返回
+	B* pb1 = (B*)pa;			   // 不安全的
+	B* pb2 = dynamic_cast<B*>(pa); // 安全的
+
+	cout << "[强制转换]pb1:" << pb1 << endl;
+	cout << "[dynamic_cast转换]pb2:" << pb2 << endl << endl;
+}
+
+//int main()
+//{
+//	A a;
+//	B b;
+//	fun(&a, "指向父类对象");
+//	fun(&b, "指向子类对象");
+//
+//	cout << typeid(a).name() << endl;
+//	decltype(a) aa;
+//
+//	function<bool(int,int)> gt = [](int x, int y){return x > y; };
+//	set<int, decltype(gt)> s;
+//
+//	return 0;
+//}
+
+
+
+
+
+
+// C++ 的 IO流
+
+
+//int main()
+//{
+//	string str;
+//	// ctrl+c 信号强杀进程
+//	// ctrl+z + 换行 流对象提取到结束标志
+//	// istream& operator>> (istream& is, string& str);
+//
+//	while (cin>>str)
+//	{
+//		cout << str << endl;
+//	}
+//
+//	// 凭什么istream的cin对象可以转bool
+//	// 凭什么string的str对象可以转bool
+//	// 因为istream的这个-》explicit operator bool() const;
+//	// 支持自定义类型转内置类型
+//	while (str)  // str 不可以直接转为 bool 
+//	{
+//		cout << str << endl;
+//	}
+//
+//	return 0;
+//}
+
+
+//class A
+//{
+//public:
+//	A(int a)
+//		:_a(a)
+//	{}
+//
+//	operator int() const
+//	{
+//		return _a;
+//	}
+//
+//private:
+//	int _a;
+//};
+
+//int main()
+//{
+//	// 内置类型转换成自定义类型
+//	//A aa1 = static_cast<A>(1);
+//	A aa1 = 1;  // 因为单参数的构造函数
+//
+//	// 自定义类转换成内置类型
+//	int x = aa1;  // 因为 operator int()
+//
+//
+//	return 0;
+//}
+
+
+
+class Date
+{
+	friend ostream& operator << (ostream& out, const Date& d);
+	friend istream& operator >> (istream& in, Date& d);
+public:
+	Date(int year = 1, int month = 1, int day = 1)
+		:_year(year)
+		, _month(month)
+		, _day(day)
+	{}
+
+	operator bool() const
+	{
+		// 这里是随意写的，假设输入_year为0，则结束
+		if (_year == 0)
+			return false;
+		else
+			return true;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+istream& operator >> (istream& in, Date& d)
+{
+	in >> d._year >> d._month >> d._day;
+	return in;
+}
+
+ostream& operator << (ostream& out, const Date& d)
+{
+	out << d._year << " " << d._month << " " << d._day;
+
+	return out;
+}
+
+
+//int main()
+//{
+//	// 自定义类型则需要我们自己重载<< 和 >>
+//	Date d(2022, 4, 10);
+//	cout << d;
+//
+//	while (d)  // str 不可以 ，Date 可以，这是因为 Date 的 operator bool()  
+//	{
+//		cin >> d;
+//		cout << d;
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+#include<fstream>
+
+//int main()
+//{
+//	std::ofstream ofs("test.txt", ofstream::out | ofstream::app);
+//	ofs << "hello world";
+//	ofs << "hello world";
+//
+//	return 0;
+//}
+
+
+struct ServerInfo
+{
+	char _address[32];
+	//string _address;
+
+	int _port;
+	Date _date;
+};
+
+struct ConfigManager
+{
+public:
+	ConfigManager(const char* filename)
+		:_filename(filename)
+	{}
+
+	// 二进制读写，读写对象中，不能有string
+	// 因为原封不动地拷贝，拷贝的是 string 的成员变量，里面是指针，除了作用域会销毁
+	void WriteBin(const ServerInfo& info)
+	{
+		ofstream ofs(_filename, ofstream::out | ofstream::binary);
+		ofs.write((char*)&info, sizeof(info));
+	}
+
+	void ReadBin(ServerInfo& info)
+	{
+		ifstream ifs(_filename, ofstream::in | ofstream::binary);
+		ifs.read((char*)&info, sizeof(info));
+	}
+
+	// 文本读写 C++文本读写更简单
+	// 文本读写本质，内存中任何类型都是转成字符串在写
+	// c语言文本读写很不方便，因为要不断转字符串
+	// c++封装了以后就有很大的优势
+	void WriteText(const ServerInfo& info)
+	{
+		ofstream ofs(_filename);
+		ofs << info._address << " ";
+		ofs << info._port << endl;
+		ofs << info._date << endl;
+	}
+
+	void ReadText(ServerInfo& info)
+	{
+		ifstream ifs(_filename);
+		ifs >> info._address;
+		ifs >> info._port;
+		ifs >> info._date;
+	}
+
+private:
+	string _filename; // 配置文件
+};
+
+
+//int main()
+//{
+//	ServerInfo winfo = { "192.168.11.189xxxxxxxxxxxxx",88,{2023,7,12} };
+//	string str;
+//	cin >> str;
+//	if (str == "二进制读")
+//	{
+//		ServerInfo rinfo;
+//		ConfigManager cm("test.txt");
+//		cm.ReadBin(rinfo);
+//		cout << rinfo._address << endl;
+//		cout << rinfo._port << endl;
+//		cout<<rinfo._date << endl;
+//	}
+//	else if (str == "二进制写")
+//	{
+//		ConfigManager cm("test.txt");
+//		cm.WriteBin(winfo);
+//	}
+//	else if (str == "文本读")
+//	{
+//		ServerInfo rinfo;
+//		ConfigManager cm("test.txt");
+//		cm.ReadText(rinfo);
+//		cout << rinfo._address << endl;
+//		cout << rinfo._port << endl;
+//		cout << rinfo._date << endl;
+//	}
+//	else if (str == "文本写")
+//	{
+//		ConfigManager cm("test.txt");
+//		cm.WriteText(winfo);
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+#include <sstream>
+
+//int main()
+//{
+//	//ostringstream oss;
+//	stringstream oss;
+//	oss << 100 << " ";
+//	oss << 11.22 << " ";
+//	oss << "hello";
+//
+//	string str = oss.str();
+//	cout << str << endl;
+//
+//	//istringstream iss(str);
+//	stringstream iss(str);
+//	int i;
+//	double d;
+//	string s;
+//	iss >> i >> d >> s;
+//	cout << i << endl;
+//	cout << d << endl;
+//	cout << s << endl;
+//
+//	return 0;
+//}
+
+
+// 序列化和反序列化
+struct ChatInfo
+{
+	string _name; // 名字
+	int _id;      // id
+	Date _date;   // 时间
+	string _msg;  // 聊天信息
+};
+
 int main()
 {
-	Singleton::GetInstance();
-	Singleton::GetInstance();
+	ChatInfo winfo = { "张三", 135246, { 2022, 4, 10 }, "晚上一起看电影吧" };
+	stringstream oss;
+	oss << winfo._name << " ";
+	oss << winfo._id << " ";
+	oss << winfo._date << " ";
+	oss << winfo._msg;
+	string str = oss.str();
+	cout << str << endl;
+
+	stringstream iss(str);
+	ChatInfo rinfo;
+	iss >> rinfo._name;
+	iss >> rinfo._id;
+	iss >> rinfo._date;
+	iss >> rinfo._msg;
+
+	cout << "-------------------------------------------------------" << endl;
+	cout << "姓名：" << rinfo._name << "(" << rinfo._id << ") ";
+	cout << rinfo._date << endl;
+	cout << rinfo._name << ":>" << rinfo._msg << endl;
+	cout << "-------------------------------------------------------" << endl;
 
 	return 0;
 }
